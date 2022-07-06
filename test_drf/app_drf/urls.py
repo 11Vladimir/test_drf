32688e30b01a3e94402547e169812483e6479449
Vladimir Novik <v.novik45@gmail.com>
2022-07-06 07:44:07 +0300

    Update app_drf

--- test_drf/app_drf/urls.py
+++ test_drf/app_drf/urls.py
@@ -3,4 +3,4 @@ from rest_framework import routers
 from app_drf import views
 
 app_router = routers.DefaultRouter()
-app_router.register("foods", views.FoodCategoryViewSet)
+app_router.register("app_drf", views.FoodCategoryViewSet)
